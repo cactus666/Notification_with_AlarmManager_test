ba0c7ef5deece6ac24946ad18c0d4059b8d12ad3
TechnoC <vt_shustiv@mail.ru>
2019-06-28 10:01:55 +0400

    .

--- app/src/main/java/com/forasoft/notification_with_alarmmanager/MainActivity.java
+++ app/src/main/java/com/forasoft/notification_with_alarmmanager/MainActivity.java
@@ -1,12 +1,18 @@
 package com.forasoft.notification_with_alarmmanager;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     private Button btn;
     private int count = 0;
@@ -20,9 +26,52 @@ public class MainActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                count++;
-                Log.i("info", count+"");
-                MyService.setServiceAlarm(MainActivity.this, true, "lol"+count);
+//                count++;
+//                Log.i("info", count+"");
+//                long time = System.currentTimeMillis();
+//                MyService.setServiceAlarm(MainActivity.this, true, "lol"+count, time);
+
+
+//                Notification notification = new NotificationCompat.Builder(MainActivity.this)
+//                        .setTicker("hello")
+//                        .setSmallIcon(R.drawable.ic_qwerty)
+//                        .setContentTitle("hi")
+//                        .setContentText("qws")
+////                .setContentIntent(pi)
+//                        .setAutoCancel(true)
+//                        .build();
+//                NotificationManagerCompat notificationManager =
+//                        NotificationManagerCompat.from(MainActivity.this);
+//                notificationManager.notify(0, notification);
+
+
+
+                NotificationCompat.Builder builder =
+                        new NotificationCompat.Builder(MainActivity.this,
+                                "56t")
+                                .setSmallIcon(R.drawable.ic_notification)
+                                .setContentTitle("Title 2")
+                                .setContentText("Notification text 2");
+
+                Notification notification = builder.build();
+
+                NotificationManager notificationManager =
+                        (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                notificationManager.notify(2, notification);
+
+
+//                NotificationCompat.Builder builder =
+//                        new NotificationCompat.Builder(MainActivity.this)
+//                                .setSmallIcon(R.drawable.ic_notification)
+//                                .setContentTitle("Notifications Example")
+//                                .setContentText("This is a test notification");
+//                Intent notificationIntent = new Intent(MainActivity.this, MainActivity.class);
+//                PendingIntent contentIntent = PendingIntent.getActivity(MainActivity.this, 0, notificationIntent,
+//                        PendingIntent.FLAG_UPDATE_CURRENT);
+//                builder.setContentIntent(contentIntent);
+                // Add as notification
+//                NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+//                manager.notify(0, builder.build());
             }
         });
     }
--- app/src/main/java/com/forasoft/notification_with_alarmmanager/MyService.java
+++ app/src/main/java/com/forasoft/notification_with_alarmmanager/MyService.java
@@ -2,11 +2,15 @@ package com.forasoft.notification_with_alarmmanager;
 
 import android.app.AlarmManager;
 import android.app.IntentService;
+import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.SystemClock;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 
 import java.util.concurrent.TimeUnit;
@@ -25,7 +29,7 @@ public class MyService extends IntentService {
         return new Intent(context, MyService.class);
     }
 
-    public static void setServiceAlarm(Context context, boolean isOn, String arg) {
+    public static void setServiceAlarm(Context context, boolean isOn, String arg, long time) {
         Log.i("info", "start_ssa");
         Intent i = MyService.newIntent(context);
         i.putExtra("val", arg);
@@ -34,8 +38,12 @@ public class MyService extends IntentService {
         AlarmManager alarmManager = (AlarmManager)
                 context.getSystemService(Context.ALARM_SERVICE);
         if (isOn) {
-            alarmManager.setRepeating(AlarmManager.ELAPSED_REALTIME,
-                    SystemClock.elapsedRealtime(), POLL_INTERVAL_MS, pi);
+            Log.i("info", "start_now");
+//            Log.i("info", (SystemClock.elapsedRealtime())+"");
+//            Log.i("info", time+"");
+            alarmManager.set(AlarmManager.ELAPSED_REALTIME, SystemClock.elapsedRealtime() + 4*60000, pi);
+//            alarmManager.setRepeating(AlarmManager.ELAPSED_REALTIME,
+//                    SystemClock.elapsedRealtime(), POLL_INTERVAL_MS, pi);
         } else {
             alarmManager.cancel(pi);
             pi.cancel();
@@ -45,6 +53,22 @@ public class MyService extends IntentService {
     @Override
     protected void onHandleIntent(Intent intent) {
         Log.i("info", "start_hi = "+intent.getStringExtra("val"));
+
+        Resources resources = getResources();
+//        Intent i = MainActivity.newIntent(this);
+//        PendingIntent pi = PendingIntent.getActivity(this, 0, i, 0);
+        Notification notification = new NotificationCompat.Builder(this)
+                .setTicker("hello")
+                .setSmallIcon(android.R.drawable.ic_menu_report_image)
+                .setContentTitle("hi")
+                .setContentText("qws")
+//                .setContentIntent(pi)
+                .setAutoCancel(true)
+                .build();
+        NotificationManagerCompat notificationManager =
+                NotificationManagerCompat.from(this);
+        notificationManager.notify(0, notification);
+
     }
 
 }
